d14699bd795b4bd71348bf8826130eaa57f2339f
zhangqingcai <zhangqingcai0815@gmail.com>
2018-11-16 15:41:13 +0800

    3rd commit, support db operation

--- readme.md
+++ readme.md
@@ -3,4 +3,6 @@
 ```
 1.使用spring boot Initializr, 使用创建spring boot 工程
 2.添加web支持、使用http://localhost:8080/greeting测试。或者直接在第一步中选中web功能
+3.添加mysql db支持、使用JPA支持ACID，使用spring的CrudRepository接口bean完成db操作。
+添加入口http://localhost:8080/demo/add?name=zhang&email=abc@abc.com， 查询使用http://localhost:8080/demo/all
 ```
---
+++ src/main/java/com/example/secondkill/dbhello/MainController.java
@@ -0,0 +1,30 @@
+package com.example.secondkill.dbhello;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping(path = "/demo")
+public class MainController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping(path="/add")
+    public @ResponseBody String addNewUser(@RequestParam String name, @RequestParam String email) {
+        User user = new User();
+        user.setName(name);
+        user.setEmail(email);
+        userRepository.save(user);
+        return "Saved";
+    }
+
+    @GetMapping(path = "/all")
+    public @ResponseBody Iterable<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+}
